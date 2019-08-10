cdec2f1f18861b19e35accee47dc1ee486758daa
Crhis35 <49347705+Crhis35@users.noreply.github.com>
2019-08-10 15:55:01 -0500

    Add files via upload

---
+++ Pagina Web/templates/js/main.js
@@ -0,0 +1,14 @@
+
+    function ConfirmDelete()
+    {
+        var respuesta = confirm("¿Estas seguro que deseas eliminar al contacto?");
+        
+        if (respuesta == true);
+        {
+            return true;
+        }
+        {
+        else {
+            return false;
+        }
+    }
